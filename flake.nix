804890bbb2a796a26c401cc2ffaf6bff024d6ee9
beni69 <73666440+beni69@users.noreply.github.com>
2023-02-05 21:02:06 +0100

    ci: upload to ghcr

--- flake.nix
+++ flake.nix
@@ -75,5 +75,26 @@
           musl-aarch64 = mkCrate { musl = true; crossSystem = "aarch64-linux"; };
           docker-aarch64 = mkDocker { musl = true; crossSystem = "aarch64-linux"; };
         };
+
+        # made for ci, for pushing the images to ghcr.io
+        apps.dockerTag = {
+          type = "app";
+          program = with import nixpkgs { inherit localSystem; };
+            let
+              version = with builtins; (fromTOML (readFile ./Cargo.toml)).package.version;
+              sk = "${skopeo}/bin/skopeo";
+              d = "${docker}/bin/docker";
+            in
+            toString (writeShellScript "dockerTag.sh" ''
+              nix build .#docker
+              ${sk} copy docker-archive:./result docker://ghcr.io/beni69/netbench:${version}-x86_64
+              
+              nix build .#docker-aarch64
+              ${sk} copy docker-archive:./result docker://ghcr.io/beni69/netbench:${version}-aarch64
+
+              ${d} manifest create ghcr.io/beni69/netbench:v${version} ghcr.io/beni69/netbench:${version}-x86_64 ghcr.io/beni69/netbench:${version}-aarch64
+              ${d} manifest push --purge ghcr.io/beni69/netbench:v${version}
+            '');
+        };
       });
 }
